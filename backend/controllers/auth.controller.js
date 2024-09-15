5135861ffad4786c6b52a045b804af1474fec222
somesh <someshsrichandan@gmail.com>
2024-09-15 23:46:32 +0530

    adding api/auth and auth routes and there controller also

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,9 @@
+export const signup = (req, res) => {
+    res.send('Hello from auth  sign up controller');
+};
+export const login = (req, res) => {
+    res.send('Hello from auth  login controller');
+};
+export const logout = (req, res) => {
+    res.send('Hello from auth logout controller');
+};
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import authRouter from './routes/auth.route.js';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use('/api/auth', authRouter);
 
 
 app.listen(PORT, () => {
