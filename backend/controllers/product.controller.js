4cc9c660aff1c5807ea6a42601ee6275cfd7a5e5
somesh <someshsrichandan@gmail.com>
2024-09-18 22:10:32 +0530

    created cloudinary and configuratiuon in also add featuerproduct route and controller

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,3 +1,4 @@
+import { redis } from '../lib/redis.js';
 import Product from '../model/product.model.js';
 
 export const getAllProducts = async (req, res) => {
@@ -9,3 +10,21 @@ export const getAllProducts = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 }
+
+export const getFeaturedProducts = async (req, res) => {
+    try {
+        const featuredProducts = await redis.get("featuredProducts");
+        if (featuredProducts) {
+            return res.json({products: JSON.parse(featuredProducts)});
+        }
+        featuredProducts = await Product.find({isFeatured: true}).lean();
+        if(!featuredProducts){
+            return res.status(404).json({message: "Featured products not found"});
+        }
+        await redis.set("featuredProducts", JSON.stringify(featuredProducts));
+        res.json(featuredProducts)
+    } catch (error) {
+        console.log("error in getFeaturedProducts controller", error);
+        res.status(500).json({ message: error.message });
+    }
+}   
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
 const router = express.Router();
-import { getAllProducts } from '../controllers/product.controller.js';
+import { getAllProducts,getFeaturedProducts } from '../controllers/product.controller.js';
 import { protectRoute , adminRoute } from '../middleware/auth.middleware.js';
 
 
 router.get('/',protectRoute , adminRoute, getAllProducts);
+router.get('/featured', getFeaturedProducts);
 
 export default router;
