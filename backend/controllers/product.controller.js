e0d2e6feff29757319144a9f13c4bb0bbdf13725
somesh <someshsrichandan@gmail.com>
2024-09-19 20:56:19 +0530

    product by catagory added

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -99,3 +99,14 @@ export const getRecommendedProducts = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 }
+
+export const getProductsByCategory = async (req, res) => {
+    const {category} = req.params;
+    try {
+        const products = await Product.find({category});
+        res.json(products);
+    } catch (error) {
+        console.log("error in getProductsByCategory controller", error);
+        res.status(500).json({ message: error.message });   
+    }
+}
