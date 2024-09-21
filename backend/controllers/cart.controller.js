79746db6fb9099968d4db971a470501dbc461b38
somesh <someshsrichandan@gmail.com>
2024-09-21 23:08:08 +0530

    add ti cart controller added

--- backend/controllers/cart.controller.js
+++ backend/controllers/cart.controller.js
@@ -1,3 +1,18 @@
 export const addToCart = async (req, res) => {
-    
+    try {
+        const {productId} = req.body;
+        const user = req.user;
+        const existingCart = await Cart.find(item => item.id=== productId);
+        if(existingCart){
+            existingCart.quantity += 1;
+        } else {
+            user.cartItems.push(productId);
+        }
+        await user.save();
+        res.json(user.cartItems);
+        
+    } catch (error) {
+        console.log("error in addToCart controller", error);
+        res.status(500).json({ message: error.message });
+    }
 }
--- backend/routes/cart.router.js
+++ backend/routes/cart.router.js
@@ -1,7 +1,11 @@
 import express from 'express';
+import { protectRoute ,getCartProducts,removeAllFromCart,updateQuantity} from '../middleware/auth.middleware';
 const router = express.Router();
 
-router.post('/' , addToCart);
+router.post('/' ,protectRoute, addToCart);
+router.get('/' ,protectRoute, getCartProducts);
+router.delete('/' ,protectRoute, removeAllFromCart);
+router.put('/' ,protectRoute, updateQuantity);
 
 
 export default router;  
