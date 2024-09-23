e6167a97bf92fff796402a5de8839a3d132cf8e3
somesh <someshsrichandan@gmail.com>
2024-09-23 22:49:14 +0530

    coupon router added

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import authRouter from './routes/auth.route.js';
 import productRouter from './routes/product.route.js';
+import cartRouter from './routes/cart.route.js';
+import couponRouter from './routes/coupon.route.js';
 import { connecDB } from './lib/db.js';
 import cookieParser from 'cookie-parser';
 
@@ -20,6 +22,7 @@ const PORT = process.env.PORT || 5000;
 app.use('/api/auth', authRouter);
 app.use('/api/products', productRouter);
 app.use('/api/cart', cartRouter);
+app.use('/api/coupons', couponRouter);
 
 
 
