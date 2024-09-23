26f9437c44d68d89bd85d8bd6ccfb7ed0ba7263e
somesh <someshsrichandan@gmail.com>
2024-09-23 23:17:21 +0530

    getcoupon controller added

--- backend/controllers/coupon.controller.js
+++ backend/controllers/coupon.controller.js
@@ -1,6 +1,9 @@
+import { Coupon } from "../model/coupon.model.js";
+
 export const getCoupon = asyncHandler(async (req, res) => {
     try {
-        
+        const coupon = await Coupon.findOne({ userId: req.user._id , isActive: true});
+        res.json(coupon || null );
     } catch (error) {
         console.log("error in getCoupon controller", error);
         res.status(500).json({ message: error.message });
