81b9a8df403b4001dbdef9fe25408172c5dd6bde
somesh <someshsrichandan@gmail.com>
2024-09-24 20:16:09 +0530

    coupon controller complete and order model added

--- backend/controllers/coupon.controller.js
+++ backend/controllers/coupon.controller.js
@@ -10,3 +10,28 @@ export const getCoupon = asyncHandler(async (req, res) => {
         
     }
 });
+
+export const validateCoupon = asyncHandler(async (req, res) => {
+    try {
+        const {code} = req.body;
+        const coupon = await Coupon.findOne({code:code,userId: req.user._id,isActive:true});
+        if(!coupon){
+            return res.status(400).json({message: "Invalid Coupon"});
+        }
+        if(coupon.expirationDate < new Date()){
+            coupon.isActive = false;
+            await coupon.save();
+            return res.status(400).json({message: "Coupon Expired"});
+        }
+        res.json({
+            message: "Coupon is Vaild",
+            code:coupon.code,
+            discountPercentage:coupon.discountPercentage
+        })
+    } catch (error) {
+        console.log("error in validateCoupon controller", error);
+        res.status(500).json({ message: error.message });
+        
+    }
+}
+);
