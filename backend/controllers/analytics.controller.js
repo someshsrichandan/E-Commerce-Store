436465641dc9f52eb503e4873d225b581d48ee95
somesh <someshsrichandan@gmail.com>
2024-09-26 23:43:35 +0530

    controller file added

---
+++ backend/controllers/analytics.controller.js
@@ -0,0 +1,8 @@
+export const getAnalyticsData = async () => {
+    try {
+        
+    } catch (error) {
+        console.log("error in getAnalyticsData", error);
+        throw new Error("Internal server error");
+    }
+}
--- backend/routes/analytics.route.js
+++ backend/routes/analytics.route.js
@@ -1,8 +1,17 @@
 import express from 'express';
 import { adminRoute, protectRoute } from '../middleware/auth.middleware';
+import getAnalyticsData from '../controllers/analytics.controller.js';
+import { get } from 'mongoose';
 const router= express.Router();
 
-router.get('/',protectRoute,adminRoute,)
+router.get('/',protectRoute,adminRoute, async (req, res) => {
+    try {
+        const analyticsData = await getAnalyticsData();
+    } catch (error) {
+        console.log("error in analytics route", error);
+        res.status(500).json({ message: error.message });
+    }
+})
 
 
 export default router;
