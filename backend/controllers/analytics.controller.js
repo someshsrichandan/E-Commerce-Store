9f0c272846f032e0d66a6315e6213ed21c703bee
somesh <someshsrichandan@gmail.com>
2024-10-01 23:51:40 +0530

    analytic some extra feastur added

--- backend/controllers/analytics.controller.js
+++ backend/controllers/analytics.controller.js
@@ -34,10 +34,49 @@ export const getAnalyticsData = async () => {
 
 export const getDailySalesData = async (startDate, endDate) => {
     try {
+       const dailSalesData = await Order.aggregate([
+        {
+            $match: {
+                createdAt: {
+                    $gte: startDate,
+                    $lt: endDate,
+                }
+            }
+        },
+        {
+            $group: {
+                _id: { $dateToString: { format: "%Y-%m-%d", date: "$createdAt" } },
+                totalSales: { $sum: 1 },
+                totalRevenue: { $sum: "$totalAmount" },
+            }
+        },
+        {
+            $sort: { _id: 1 }
+        }
+       ])
+       const dataArray = getDataInRange(startDate, endDate);
+       return dataArray.map((date) => {
+           const data = dailSalesData.find((data) => data._id === date) || { totalSales: 0, totalRevenue: 0 };
+           return {
+               date,
+                totalSales: foundData?.sales || 0,
+                revenue: foundData?.revenue || 0,
+           }
+       })
         
     } catch (error) {
         console.log("error in getDailySalesData", error);
         throw new Error("Internal server error");
         
     }
+}
+
+function getDataInRange(startDate, endDate) {
+    const dateArray = [];
+    const currentDate = new Date(startDate);
+    while (currentDate <= endDate) {
+        dates.push(currentDate.toISOString().slice(0, 10));
+        currentDate.setDate(currentDate.getDate() + 1);
+    }
+    return dateArray;
 }
